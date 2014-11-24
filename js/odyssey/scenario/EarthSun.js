8b68e6334fff735785cff3f85472e9deafdd25cf
Abhishek <abhishek.mech011@gmail.com>
2014-11-24 14:16:56 +0530

    Added equator in Seasons Environment

--- js/odyssey/scenario/EarthSun.js
+++ js/odyssey/scenario/EarthSun.js
@@ -11,6 +11,11 @@ define(
 			name : 'EarthSun',
 			title : 'पृथ्वी के मौसम',
 			commonBodies : ['earth', 'sun'],
+			bodies:{
+				earth:{
+					map : 'img/earthmap1k_KSC.jpg'
+				}
+			},
 			secondsPerTick : {min: 3600, max: 3600 * 15, initial:3600 * 5},//3600 * 24 * 2,
 			//secondsPerTick : {min: 3600/2, max: 3600 * 5, initial:3600},
 			defaultGuiSettings : { 
